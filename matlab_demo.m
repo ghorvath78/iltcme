2eb0357a86b7001e10663c2e51d7fbdcb27615ee
Gabor Horvath <ghorvath@hit.bme.hu>
2019-01-25 17:28:08 +0100

    initial commit

---
+++ matlab_demo.m
@@ -0,0 +1,58 @@
+tFuns = [];
+
+tFuns(1).name="exponential";
+tFuns(1).lt=@(s)1/(1+s);
+tFuns(1).ilt=@(x)exp(-x);
+tFuns(1).xvals=0.1:0.01:5;
+tFuns(1).yrange=[-0.5,1.5];
+
+tFuns(2).name="sine";
+tFuns(2).lt=@(s)1/(1+s^2);
+tFuns(2).ilt=@(x)sin(x);
+tFuns(2).xvals=0.1:0.05:15;
+tFuns(2).yrange=[-1.2,1.2];
+
+tFuns(3).name="heavyside";
+tFuns(3).lt=@(s)exp(-s)/s;
+tFuns(3).ilt=@(x)floor(x>1);
+tFuns(3).xvals=0.1:0.01:3;
+tFuns(3).yrange=[-0.5,1.5];
+
+tFuns(4).name="expheavyside";
+tFuns(4).lt=@(s)exp(-s)/(1+s);
+tFuns(4).ilt=@(x)floor(x>1)*exp(1-x);
+tFuns(4).xvals=0.1:0.01:5;
+tFuns(4).yrange=[-0.5,1.2];
+
+tFuns(5).name="squarewave";
+tFuns(5).lt=@(s)(1/s)/(exp(s)+1);
+tFuns(5).ilt=@(x)mod(floor(x),2);
+tFuns(5).xvals=0.1:0.02:10;
+tFuns(5).yrange=[-1,2];
+
+tFuns(6).name="staircase";
+tFuns(6).lt=@(s)(1/s)/(exp(s)-1);
+tFuns(6).ilt=@(x)floor(x);
+tFuns(6).xvals=0.1:0.01:5;
+tFuns(6).yrange=[-1,5];
+
+
+% Set the parameters of the DEMO here:
+testFunction = "squarewave";
+funEvals = 100;
+
+% compute the inverse and plot the comparison
+ix = 1;
+while(tFuns(ix).name~=testFunction)
+    ix = ix + 1;
+end
+
+cme = ilt(tFuns(ix).lt, tFuns(ix).xvals, funEvals, "cme");
+euler = ilt(tFuns(ix).lt, tFuns(ix).xvals, funEvals, "euler");
+gaver = ilt(tFuns(ix).lt, tFuns(ix).xvals, funEvals, "gaver");
+exact = arrayfun(tFuns(ix).ilt, tFuns(ix).xvals)';
+
+plot([exact, cme, euler, gaver])
+ylim(tFuns(ix).yrange)
+legend("exact", "cme", "euler", "gaver")
+
---
+++ matlab_ilt.m
@@ -0,0 +1,64 @@
+function ilt = ilt(fun, T, maxFnEvals, method)
+
+global cmeParams;
+if isempty(cmeParams)
+    cmeParams = jsondecode(fileread('iltcme.json'));
+end
+if ~exist('method','var')
+    method = 'cme';
+end
+
+if strcmp(method, 'cme')
+
+    % find the most steep CME satisfying maxFnEvals
+    params = cmeParams(1);
+    for i=2:length(cmeParams)
+        if cmeParams(i).cv2<params.cv2 && cmeParams(i).n+1<=maxFnEvals
+            params = cmeParams(i);
+        end
+    end
+
+    % compute eta and beta parameters
+    eta = [params.c*params.mu1, params.a'*params.mu1 + 1i*params.b'*params.mu1];
+    beta = [1, 1 + 1i*(1:params.n)*params.omega] * params.mu1;
+
+elseif strcmp(method,'euler')
+    
+    n_euler = floor((maxFnEvals-1)/2);
+    eta = [0.5, ones(1, n_euler), zeros(1, n_euler-1), 2^-n_euler];
+    for k = 1:n_euler-1
+%        eta(2*n_euler-k + 1) = eta(2*n_euler-k + 2) + 2^-n_euler * nchoosek(n_euler, k);
+       eta(2*n_euler-k + 1) = eta(2*n_euler-k + 2) + exp(sum(log(1:n_euler)) - n_euler*log(2) - sum(log(1:k)) - sum(log(1:(n_euler-k))));
+    end
+    k = 0:2*n_euler;
+    beta = n_euler*log(10)/3 + 1i*pi*k;
+    eta  = (10^((n_euler)/3))*(1-mod(k, 2)*2) .* eta;  
+    
+elseif strcmp(method,'gaver')
+
+    if mod(maxFnEvals,2)==1
+        maxFnEvals = maxFnEvals - 1;
+    end
+    ndiv2 = maxFnEvals/2;
+    eta = zeros(1,maxFnEvals);
+    beta = zeros(1,maxFnEvals);
+    for k = 1:maxFnEvals % itration index
+        inside_sum = 0.0;
+        for j = floor((k+1)/2):min(k,ndiv2) %  eta summation index
+%            inside_sum=inside_sum+((j^((ndiv2+1))/factorial(ndiv2))*(nchoosek(ndiv2, j)*nchoosek(2*j, j)*nchoosek(j, k-j)));           
+           inside_sum=inside_sum+exp((ndiv2+1)*log(j) - sum(log(1:(ndiv2-j))) + sum(log(1:2*j)) - 2*sum(log(1:j)) - sum(log(1:(k-j))) - sum(log(1:(2*j-k))));
+        end
+        eta(k)=log(2.0)*(-1)^(k+ndiv2)*inside_sum;
+        beta(k) = k * log(2.0);
+    end    
+    
+else
+    error('This inverse laplace transform method is unknown. Supported ones: cme, euler, gaver');
+end
+
+% common part for all abate-whitt variants
+[eta_mesh,T_mesh]= meshgrid(eta, T);
+beta_mesh = meshgrid(beta, T);
+ilt = 1./reshape(T,[],1) .* sum(real(eta_mesh .* arrayfun(fun, beta_mesh./T_mesh)), 2);
+
+end
